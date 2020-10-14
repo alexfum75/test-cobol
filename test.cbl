00a1bd09767a4fb10b1455f8996ee31d16588b6e
Alessandro <44378851+alexfum75@users.noreply.github.com>
2020-10-14 09:28:50 +0200

    Add files via upload

---
+++ test.cbl
@@ -0,0 +1,14 @@
+       IDENTIFICATION DIVISION.
+      *> Author: Alessandro
+      *> Date: 2020-10-14
+       PROGRAM-ID. TEST-PROGRAM.
+       ENVIRONMENT DIVISION.
+       CONFIGURATION SECTION.
+       SOURCE-COMPUTER. DELL.
+       OBJECT-COMPUTER. DELL.
+       DATA DIVISION.
+       FILE SECTION.
+       WORKING-STORAGE SECTION.
+       PROCEDURE DIVISION.
+           DISPLAY "Hello world"
+           STOP RUN.
